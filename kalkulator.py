c430c4f5b2e641ced86f1f9eb552d5f49010a804
wuefi <wuefiak.op.pl>
2026-04-03 02:04:02 +0200

    potgowanie o wykadniku dowolnym i procenty

--- kalkulator.py
+++ kalkulator.py
@@ -4,7 +4,7 @@ print("--- Witaj w Kalkulatorek v2.0! ---")
 
 while True:
     print("\n--- Nowe obliczenie ---")
-    print("Wybierz działanie: +, -, *, /, **, p (pierwiastek), k (pole kola) lub wpisz 'x' (wyjdź):")
+    print("Wybierz działanie: +, -, *, /, **, p (pierwiastek), % (obliczanie %), k (pole kola) lub wpisz 'x' (wyjdź):")
     dzialanie = input("Twój wybór: ")
 
     # Sprawdzamy, czy użytkownik chce wyjść
@@ -30,13 +30,31 @@ while True:
             a = float(input("Podaj promień koła (r): "))
             wynik = math.pi * (a ** 2)
             print(f"Użyta wartość Pi:  {math.pi:.2f}")
-            print(f"Wynik: {wynik: .2f}")
+            print(f"Wynik: {wynik: .2f} ")
         except ValueError:
             print("To nie jest liczba! Kolejna próba")
         continue
 
+    elif dzialanie == "%":
+        try:
+            a = float(input("Ile procent (np. 15): "))
+            b = float(input("Z jakiej liczby: "))
+                # Wzór: (procent / 100) * liczba
+            wynik = (a / 100) * b
+            print(f"Wynik: {wynik: .2f}")
+        except ValueError:
+                print("Błąd! Podaj poprawne liczby.")
+        continue
+    elif dzialanie == "**":
+        try:
+            a = float(input("Podaj podstawę potęgi: "))
+            b = float(input("Podaj wykładnik (do której potęgi): "))
+            wynik = a ** b
+        except ValueError:
+            print("Błąd! Podaj poprawne liczby.")
+            continue    
     else:
-        # Dla pozostałych działań (+, -, *, /, **)
+        # Dla pozostałych działań (+, -, *, /)
         try:
             a = float(input("Podaj pierwszą liczbę: "))
             b = float(input("Podaj drugą liczbę: "))
@@ -53,7 +71,7 @@ while True:
         elif dzialanie == "/":
             wynik = a / b if b != 0 else "Nie dziel przez zero!"
         elif dzialanie == "**":
-            wynik = a ** b
+            wynik = a ** b        
         else:
             wynik = "Nieznane działanie"
 
